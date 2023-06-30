37bf37c1396ad8a78880e0a569ffe345b4de046d
Dedman <dimas234325325@gmail.com>
2023-06-30 12:07:47 +0000

    Revert "isnt work"
    This reverts commit e9087e07329271432e75b051d29bb9a7bda21edf.

--- betelgeuse/paper/forms.py
+++ betelgeuse/paper/forms.py
@@ -1,64 +1,25 @@
 from django import forms
-from .models import Paper, Report, Image, StructureResearch, RfaResearch, FurieResearch, KrsResearch
+from .models import *
+from django.forms import inlineformset_factory
 
 
 class ReportForm(forms.ModelForm):
     class Meta:
         model = Report
-        fields = ["date", "file"]
+        fields = "__all__"
 
 
-class ImageForm(forms.ModelForm):
-    class Meta:
-        model = Image
-        fields = ["bw_positive", "color_positive"]
-
-
-class StructureResearchForm(forms.ModelForm):
-    class Meta:
-        model = StructureResearch
-        fields = ["hertzberg", "graf_c", "discription"]
-
-
-class RfaResearchForm(forms.ModelForm):
-    class Meta:
-        model = RfaResearch
-        fields = ["image", "txt", "parameters", "result"]
-
-
-class FurieResearchForm(forms.ModelForm):
-    class Meta:
-        model = FurieResearch
-        fields = ["image", "txt", "parameters", "result"]
-
-
-class KrsResearchForm(forms.ModelForm):
-    class Meta:
-        model = KrsResearch
-        fields = ["image", "txt", "parameters", "result"]
+PaperReportFormSet = inlineformset_factory(Paper, Report, form=ReportForm, extra=1)
 
 
 class PaperForm(forms.ModelForm):
-    reports = forms.formsets.BaseFormSet(form=ReportForm, prefix="report_form")
-    images = forms.formsets.BaseFormSet(form=ImageForm, prefix="image_form")
-    structure_researches = forms.formsets.BaseFormSet(form=StructureResearchForm, prefix="structure_form")
-    rfa_researches = forms.formsets.BaseFormSet(form=RfaResearchForm, prefix="rfa_form")
-    furie_researches = forms.formsets.BaseFormSet(form=FurieResearchForm, prefix="furie_form")
-    krs_researches = forms.formsets.BaseFormSet(form=KrsResearchForm, prefix="krs_form")
+    report_formset = PaperReportFormSet(prefix="reports")
 
     class Meta:
         model = Paper
-        fields = [
-            "code",
-            "title",
-            "year_start",
-            "year_end",
-            "url",
-            "subtitles",
-            "authors",
-            "year_restoration",
-            "passepartout",
-            "thickness",
-            "ph",
-            "HimImage",
-        ]
+        fields = "__all__"
+
+        widgets = {
+            "date": forms.DateInput(attrs={"required": "required", "type": "date"}),
+            "file": forms.ClearableFileInput(attrs={"required": "required"}),
+        }
--- betelgeuse/paper/views.py
+++ betelgeuse/paper/views.py
@@ -13,85 +13,24 @@ def index(request):
 
 
 def addPaper(request):
-    ReportFormSet = forms.formsets.formset_factory(ReportForm, extra=1)
-    ImageFormSet = forms.formsets.formset_factory(ImageForm, extra=1)
-    StructureResearchFormSet = forms.formsets.formset_factory(StructureResearchForm, extra=1)
-    RfaResearchFormSet = forms.formsets.formset_factory(RfaResearchForm, extra=1)
-    FurieResearchFormSet = forms.formsets.formset_factory(FurieResearchForm, extra=1)
-    KrsResearchFormSet = forms.formsets.formset_factory(KrsResearchForm, extra=1)
-
+    # if this is a POST request we need to process the form data
     if request.method == "POST":
-        form = PaperForm(request.POST, request.FILES)
-        reports_formset = ReportFormSet(request.POST, prefix="report_form")
-        images_formset = ImageFormSet(request.POST, request.FILES, prefix="image_form")
-        structure_formset = StructureResearchFormSet(request.POST, request.FILES, prefix="structure_form")
-        rfa_formset = RfaResearchFormSet(request.POST, request.FILES, prefix="rfa_form")
-        furie_formset = FurieResearchFormSet(request.POST, request.FILES, prefix="furie_form")
-        krs_formset = KrsResearchFormSet(request.POST, request.FILES, prefix="krs_form")
+        form = PaperForm(request.POST)
+        report_formset = PaperReportFormSet(request.POST, request.FILES, prefix="reports")
 
-        if (
-            form.is_valid()
-            and reports_formset.is_valid()
-            and images_formset.is_valid()
-            and structure_formset.is_valid()
-            and rfa_formset.is_valid()
-            and furie_formset.is_valid()
-            and krs_formset.is_valid()
-        ):
-            paper = form.save(commit=False)
-            paper.save()
-
-            for report_form in reports_formset:
-                report = report_form.save(commit=False)
-                report.paper = paper
-                report.save()
-
-            for image_form in images_formset:
-                image = image_form.save(commit=False)
-                image.paper = paper
-                image.save()
-
-            for structure_form in structure_formset:
-                structure = structure_form.save(commit=False)
-                structure.paper = paper
-                structure.save()
-
-            for rfa_form in rfa_formset:
-                rfa = rfa_form.save(commit=False)
-                rfa.paper = paper
-                rfa.save()
-
-            for furie_form in furie_formset:
-                furie = furie_form.save(commit=False)
-                furie.paper = paper
-                furie.save()
-
-            for krs_form in krs_formset:
-                krs = krs_form.save(commit=False)
-                krs.paper = paper
-                krs.save()
+        if form.is_valid() and report_formset.is_valid():
+            paper = form.save()
+            report_formset.instance = paper
+            report_formset.save()
 
             return index(request)
-
+        else:
+            print(report_formset.errors)
     else:
         form = PaperForm()
-        reports_formset = ReportFormSet(prefix="report_form")
-        images_formset = ImageFormSet(prefix="image_form")
-        structure_formset = StructureResearchFormSet(prefix="structure_form")
-        rfa_formset = RfaResearchFormSet(prefix="rfa_form")
-        furie_formset = FurieResearchFormSet(prefix="furie_form")
-        krs_formset = KrsResearchFormSet(prefix="krs_form")
+        report_formset = PaperReportFormSet(prefix="reports")
 
-    context = {
-        "form": form,
-        "reports_formset": reports_formset,
-        "images_formset": images_formset,
-        "structure_formset": structure_formset,
-        "rfa_formset": rfa_formset,
-        "furie_formset": furie_formset,
-        "krs_formset": krs_formset,
-    }
-    return render(request, "paper/addPaper.html", context)
+    return render(request, "paper/addPaper.html", {"form": form, "report_formset": report_formset})
 
 
 def about(request):
